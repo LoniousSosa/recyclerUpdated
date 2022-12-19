f6f731c525c3dc1953a59dfbb0fd1762bc65bd37
LoniousSosa <lsosa19@jaumebalmes.net>
2022-12-19 19:38:55 +0100

    Recycler animado y decorado - Lonious

--- app/src/main/java/com/example/myfirsappm07/AdaptadorRecicler.java
+++ app/src/main/java/com/example/myfirsappm07/AdaptadorRecicler.java
@@ -1,21 +1,26 @@
 package com.example.myfirsappm07;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
+import java.util.ArrayList;
+
 public class AdaptadorRecicler extends Adapter<AdaptadorRecicler.TitularesViewHolder>{
-    private final Titular[] datos;
+    private final ArrayList<Titular> datos;
     Context context;
 
-    public AdaptadorRecicler(Titular[] datos) {
+    public AdaptadorRecicler(ArrayList<Titular> datos) {
         super();
         this.datos = datos;
     }
@@ -25,6 +30,7 @@ public class AdaptadorRecicler extends Adapter<AdaptadorRecicler.TitularesViewHo
         private final TextView lblTitulo;
         private final TextView lblSubtitulo;
         private final View item;
+
 
         public TitularesViewHolder(View item) {
             super(item);
@@ -44,23 +50,35 @@ public class AdaptadorRecicler extends Adapter<AdaptadorRecicler.TitularesViewHo
         public void bindTitular(Titular titular) {
             this.lblTitulo.setText(titular.getTitulo());
             this.lblSubtitulo.setText(titular.getSubtitulo());
+
         }
     }
 
 
     public AdaptadorRecicler.TitularesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LinearLayout item = (LinearLayout) LayoutInflater.from
-                (parent.getContext()).inflate(R.layout.visual_recicler, parent, false);;
+                (parent.getContext()).inflate(R.layout.visual_recicler, parent, false);
         return (new AdaptadorRecicler.TitularesViewHolder((View) item));
     }
 
     @Override
     public void onBindViewHolder(TitularesViewHolder holder, int position) {
-        Titular titular = datos[position];
+        Titular titular = datos.get(position);
         holder.bindTitular(titular);
+        holder.lblTitulo.startAnimation(AnimationUtils.loadAnimation(
+                holder.item.getContext(),R.anim.animation_one
+        ));
+
+        holder.lblSubtitulo.startAnimation(AnimationUtils.loadAnimation(
+                holder.item.getContext(),R.anim.animation_one
+        ));
+
+        holder.itemView.setOnClickListener(view -> {
+            Log.d("Se ha clicado el boton:",datos.get(position).getTitulo());
+        });
     }
 
     public int getItemCount() {
-        return this.datos.length;
+        return this.datos.size();
     }
 }
--- app/src/main/java/com/example/myfirsappm07/ReciclerLonious.java
+++ app/src/main/java/com/example/myfirsappm07/ReciclerLonious.java
@@ -1,30 +1,73 @@
 package com.example.myfirsappm07;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ReciclerLonious extends AppCompatActivity {
 
     RecyclerView recView;
-    Titular[]datos;
+    ArrayList<Titular> datos;
     AdaptadorRecicler adaptador;
+    Button botonInsertar;
+    Button botonEliminar;
+    Button botonMover;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recicler_lonious);
+        datos = new ArrayList<>();
+        botonInsertar = findViewById(R.id.insertar);
+        botonMover = findViewById(R.id.mover);
+        botonEliminar = findViewById(R.id.eliminar);
         recView = findViewById(R.id.recView);
         recView.setHasFixedSize(true);
-        recView.setLayoutManager(
-                        (new LinearLayoutManager
-                                (this, LinearLayoutManager.VERTICAL, false)));
-        datos = new Titular[50];
-        for (int i = 0; i < datos.length; i++) {
-            datos[i] = new Titular("Título "+(i+1),"Subtítulo " +(i+1));
+        recView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recView.setLayoutManager((new GridLayoutManager(this,3)));
+        for (int i =0; i < 50; i++) {
+            datos.add(i,new Titular("Título "+(i+1),"Subtítulo " +(i+1)));
         }
         adaptador = new AdaptadorRecicler(datos);
         recView.setAdapter(adaptador);
+
+        botonInsertar.setOnClickListener(view -> {
+            int size = datos.size();
+            datos.add(size,new Titular("Título "+(size+1),"Subtítulo " +(size+1)));
+            adaptador = new AdaptadorRecicler(datos);
+            recView.setAdapter(adaptador);
+            Log.d("Se ha añadido un Título","");
+        });
+
+        botonEliminar.setOnClickListener(view -> {
+            int size = datos.size();
+            datos.remove(size-1);
+            adaptador = new AdaptadorRecicler(datos);
+            recView.setAdapter(adaptador);
+            Log.d("Se eliminó el Título:",Integer.toString(size));
+        });
+
+        botonMover.setOnClickListener(view -> {
+            int position1 = (int)(Math.random()*datos.size());
+            int position2 = (int)(Math.random()*datos.size());
+            System.out.println(position2);
+            Titular prueba = datos.get(position1);
+            datos.set(position1, datos.get(position2));
+            datos.set(position2,prueba);
+            adaptador = new AdaptadorRecicler(datos);
+            recView.setAdapter(adaptador);
+            Log.d("Se movió el Título:",Integer.toString(position1));
+        });
     }
 }
